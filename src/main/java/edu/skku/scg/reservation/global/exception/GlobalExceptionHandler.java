a94f4e0be78bdf3730ed7f096cd139a317a58f80
snghyn28 <qazxsw100415@gmail.com>
2026-03-16 14:49:02 +0900

    feat: 예외 처리 로직 추가

---
+++ src/main/java/edu/skku/scg/reservation/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,47 @@
+package edu.skku.scg.reservation.global.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jspecify.annotations.NonNull;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    protected ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
+        log.warn("Business Exception", e);
+        return ResponseEntity
+                .status(e.getErrorCode().getStatus())
+                .body(ErrorResponse.from(e.getErrorCode()));
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(
+            Exception e,
+            Object body,
+            @NonNull HttpHeaders headers,
+            HttpStatusCode statusCode, @NonNull WebRequest request) {
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                "SPRING-" + statusCode.value(),
+                e.getMessage()
+        );
+
+        return ResponseEntity.status(statusCode).body(errorResponse);
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error("Internal Server Error", e);
+        return ResponseEntity
+                .status(ErrorCode.SERVER_ERROR.getStatus())
+                .body(ErrorResponse.from(ErrorCode.SERVER_ERROR));
+    }
+}
