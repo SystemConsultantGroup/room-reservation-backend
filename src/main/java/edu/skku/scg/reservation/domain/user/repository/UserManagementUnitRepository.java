76ca3bb41ba747250a267ace4341b0f8a65b2efd
snghyn28 <qazxsw100415@gmail.com>
2026-03-19 17:39:13 +0900

    fix: UserManagementUnitRepository의 상속 인터페이스 타입 파라미터를 User에서 UserManagementUnit로 변경

--- src/main/java/edu/skku/scg/reservation/domain/user/repository/UserManagementUnitRepository.java
+++ src/main/java/edu/skku/scg/reservation/domain/user/repository/UserManagementUnitRepository.java
@@ -1,12 +1,12 @@
 package edu.skku.scg.reservation.domain.user.repository;
 
-import edu.skku.scg.reservation.domain.user.entity.User;
+import edu.skku.scg.reservation.domain.user.entity.UserManagementUnit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface UserManagementUnitRepository extends JpaRepository<User, Long> {
+public interface UserManagementUnitRepository extends JpaRepository<UserManagementUnit, Long> {
 
     @Query("SELECT umu.managementUnit.id " +
             "FROM UserManagementUnit umu " +
