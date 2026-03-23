eb562a07ebbbce9c9bb9d7fb8f25ec6e059b5f9a
snghyn28 <qazxsw100415@gmail.com>
2026-03-23 14:22:13 +0900

    feat: OnboardingRequestDto의 majors 필드에 @Valid 어노테이션 추가

--- src/main/java/edu/skku/scg/reservation/domain/auth/dto/OnboardingRequestDto.java
+++ src/main/java/edu/skku/scg/reservation/domain/auth/dto/OnboardingRequestDto.java
@@ -2,6 +2,7 @@ package edu.skku.scg.reservation.domain.auth.dto;
 
 import edu.skku.scg.reservation.domain.user.entity.MajorType;
 import edu.skku.scg.reservation.domain.user.entity.UserType;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
@@ -12,6 +13,7 @@ public record OnboardingRequestDto(
         @NotNull
         UserType userType,
 
+        @Valid
         List<MajorRequest> majors
 ) {
         public record MajorRequest(
