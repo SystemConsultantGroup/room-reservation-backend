a69d9825359f8dc09cbfd1fea197750d568c21e4
snghyn28 <qazxsw100415@gmail.com>
2026-03-23 12:34:07 +0900

    feat: @AdminApi에서 관리권한 있는지 체크

--- src/main/java/edu/skku/scg/reservation/global/annotation/AdminApi.java
+++ src/main/java/edu/skku/scg/reservation/global/annotation/AdminApi.java
@@ -1,5 +1,7 @@
 package edu.skku.scg.reservation.global.annotation;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -7,5 +9,6 @@ import java.lang.annotation.Target;
 
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("principal.managingUnitIds != null && !principal.managingUnitIds.isEmpty()")
 public @interface AdminApi {
 }
