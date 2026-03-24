98a6ce60e1d91e20b4c067cb6e62792466d1812f
snghyn28 <qazxsw100415@gmail.com>
2026-03-24 18:10:29 +0900

    feat: NotBlank -> NotEmpty로 수정

--- src/main/java/edu/skku/scg/reservation/domain/room/dto/RoomUpdateRequestDto.java
+++ src/main/java/edu/skku/scg/reservation/domain/room/dto/RoomUpdateRequestDto.java
@@ -2,6 +2,7 @@ package edu.skku.scg.reservation.domain.room.dto;
 
 import edu.skku.scg.reservation.domain.room.entity.RoomAccessPolicy;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
@@ -21,6 +22,6 @@ public record RoomUpdateRequestDto(
         @Positive
         Integer maxBookingMinutes,
 
-        @NotBlank
+        @NotEmpty
         List<Long> majorIds
 ) {}
