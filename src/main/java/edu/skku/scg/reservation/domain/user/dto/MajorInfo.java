323f0c4ebf6ecda5fff57a8130f6969e0fe0b9a9
snghyn28 <qazxsw100415@gmail.com>
2026-03-25 17:51:09 +0900

    feat: MajorInfo의 id 필드의 타입을 Long으로 변경

--- src/main/java/edu/skku/scg/reservation/domain/user/dto/MajorInfo.java
+++ src/main/java/edu/skku/scg/reservation/domain/user/dto/MajorInfo.java
@@ -3,7 +3,7 @@ package edu.skku.scg.reservation.domain.user.dto;
 import edu.skku.scg.reservation.domain.user.entity.MajorType;
 
 public record MajorInfo (
-    Integer id,
+    Long id,
     String name,
     MajorType type
 ) {
