8bc415e6681174f45fbac53b6f7d3e5e7419d2b0
snghyn28 <qazxsw100415@gmail.com>
2026-03-25 18:05:50 +0900

    feat: 공지 수정 API 추가

--- src/main/java/edu/skku/scg/reservation/domain/organization/controller/NoticeController.java
+++ src/main/java/edu/skku/scg/reservation/domain/organization/controller/NoticeController.java
@@ -1,10 +1,12 @@
 package edu.skku.scg.reservation.domain.organization.controller;
 
 import edu.skku.scg.reservation.domain.organization.dto.NoticeDetailDto;
+import edu.skku.scg.reservation.domain.organization.dto.UpdateNoticeRequestDto;
 import edu.skku.scg.reservation.global.annotation.PublicApi;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +20,11 @@ public class NoticeController {
     @GetMapping
     public NoticeDetailDto getNotice() {
         return null;
+    }
+
+    @Operation(summary = "공지 수정")
+    @PublicApi
+    @PutMapping
+    public void updateNotice(UpdateNoticeRequestDto dto) {
     }
 }
