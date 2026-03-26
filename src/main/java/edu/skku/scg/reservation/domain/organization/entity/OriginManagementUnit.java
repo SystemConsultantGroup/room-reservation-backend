4db71dc7befd9d9d72db4f52c4a87e6ef6c7ac2d
snghyn28 <qazxsw100415@gmail.com>
2026-03-26 15:12:06 +0900

    feat: originUrl 필드에 nullable = false, unique = true 추가

--- src/main/java/edu/skku/scg/reservation/domain/organization/entity/OriginManagementUnit.java
+++ src/main/java/edu/skku/scg/reservation/domain/organization/entity/OriginManagementUnit.java
@@ -15,6 +15,7 @@ public class OriginManagementUnit extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String originUrl;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
