795fe46189d9fa848307ff93f3137360beb3e3a4
snghyn28 <qazxsw100415@gmail.com>
2026-03-30 16:41:36 +0900

    fix: findManagementUnitIdByOriginUrl에 명시적 query 추가

--- src/main/java/edu/skku/scg/reservation/domain/organization/repository/OriginManagementUnitRepository.java
+++ src/main/java/edu/skku/scg/reservation/domain/organization/repository/OriginManagementUnitRepository.java
@@ -3,11 +3,13 @@ package edu.skku.scg.reservation.domain.organization.repository;
 import edu.skku.scg.reservation.domain.organization.entity.OriginManagementUnit;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface OriginManagementUnitRepository extends JpaRepository<OriginManagementUnit, Long> {
 
     @Cacheable(cacheNames = "originManagementUnitId", key = "#originUrl")
+    @Query("SELECT o.managementUnit.id FROM OriginManagementUnit o WHERE o.originUrl = :originUrl")
     Optional<Long> findManagementUnitIdByOriginUrl(String originUrl);
 }
